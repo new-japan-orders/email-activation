5a0f83c4aeef5bcf10c721c138c4a36cbaf59bd2
emile-yamaji <yamaji_daisuke@1stbit.co.jp>
2018-05-29 03:17:22 +0000

    Changes to be committed: 	new file:   ServiceProvider.php

---
+++ ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NewJapanOrders\EmailActivation;
+
+use Illuminate\Support\ServiceProvider as Provider;
+
+class ServiceProvider extends Provider {
+
+    /** 
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {   
+    }   
+
+    /** 
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {   
+    }   
+}
